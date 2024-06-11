72dee00f28918c958d3fe28318bfa216d273fe7e
asperwada <134709746+asperwada@users.noreply.github.com>
2024-06-11 21:13:34 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 let counter = 0;
 let purchaseCount = 0;
+let stamina = 3000;
+const maxStamina = 2;
 const purchaseCost = 5;  // Установите требуемую сумму для покупки
+const recoveryTimeInMinutes = 0.1;  // Время восстановления в минутах
 
 document.getElementById('counterButton').addEventListener('click', () => {
-    counter++;
-    document.getElementById('counterButton').innerText = counter;
+    if (stamina > 0) {
+        counter++;
+        stamina--;
+        document.getElementById('counterButton').innerText = counter;
+        document.getElementById('stamina').innerText = `Stamina: ${stamina}`;
+    } else {
+        alert("Недостаточно выносливости для клика. Подождите восстановления.");
+    }
 });
 
 document.getElementById('rectButton').addEventListener('click', () => {
@@ -17,3 +26,12 @@ document.getElementById('rectButton').addEventListener('click', () => {
         alert("Недостаточно средств для покупки");
     }
 });
+
+// Восстановление выносливости каждые recoveryTimeInMinutes минут
+function restoreStamina() {
+    stamina = maxStamina;
+    document.getElementById('stamina').innerText = `Stamina: ${stamina}`;
+}
+
+// Установить таймер для восстановления выносливости
+setInterval(restoreStamina, recoveryTimeInMinutes * 60 * 1000); // Переводим минуты в миллисекунды
