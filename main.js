19253ac7fe5d75f58c2c99d19a7672d1fbfbbffe
asperwada <134709746+asperwada@users.noreply.github.com>
2024-06-11 21:36:24 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let counter = 0;
 let purchaseCount = 0;
 let stamina = 3000;
 const maxStamina = 3000;
-const purchaseCost = 5;  // Установите требуемую сумму для покупки
+let purchaseCost = 5;  // Начальная стоимость покупки
 const recoveryTimeInMinutes = 60;  // Время восстановления в минутах
 
 document.getElementById('counterButton').addEventListener('click', () => {
@@ -10,7 +10,6 @@ document.getElementById('counterButton').addEventListener('click', () => {
         counter++;
         stamina--;
         document.getElementById('counterButton').innerText = counter;
-        document.getElementById('stamina').innerText = `Stamina: ${stamina}`;
         document.getElementById('staminaDisplay').innerText = `Stamina: ${stamina}`;
     } else {
         alert("Недостаточно выносливости для клика. Подождите восстановления.");
@@ -21,8 +20,10 @@ document.getElementById('rectButton').addEventListener('click', () => {
     if (counter >= purchaseCost) {
         counter -= purchaseCost;
         purchaseCount++;
+        purchaseCost *= 2;  // Удваиваем стоимость покупки
         document.getElementById('counterButton').innerText = counter;
         document.getElementById('rectButton').innerText = `Купить X ${purchaseCount}`;
+        document.getElementById('price').innerText = `Цена: ${purchaseCost}`;
     } else {
         alert("Недостаточно средств для покупки");
     }
@@ -31,7 +32,6 @@ document.getElementById('rectButton').addEventListener('click', () => {
 // Восстановление выносливости каждые recoveryTimeInMinutes минут
 function restoreStamina() {
     stamina = maxStamina;
-    document.getElementById('stamina').innerText = `Stamina: ${stamina}`;
     document.getElementById('staminaDisplay').innerText = `Stamina: ${stamina}`;
 }
 
