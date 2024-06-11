598cc043b6ba2becf1d143de0ffc74f6db5a1489
asperwada <134709746+asperwada@users.noreply.github.com>
2024-06-11 21:01:33 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 let counter = 0;
+let purchaseCount = 0;
+const purchaseCost = 5;  // Установите требуемую сумму для покупки
 
 document.getElementById('counterButton').addEventListener('click', () => {
     counter++;
     document.getElementById('counterButton').innerText = counter;
+});
+
+document.getElementById('rectButton').addEventListener('click', () => {
+    if (counter >= purchaseCost) {
+        counter -= purchaseCost;
+        purchaseCount++;
+        document.getElementById('counterButton').innerText = counter;
+        document.getElementById('rectButton').innerText = `Купить X ${purchaseCount}`;
+    } else {
+        alert("Недостаточно средств для покупки");
+    }
 });
